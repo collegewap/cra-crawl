c269b6ffd8cf0e1cbd2d7ef007809cce6be73a24
shruthias <shruthias1996@gmail.com>
2023-01-06 16:52:28 +0530

    added fruit list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fruits</h1>
+      <ul className="fruits-list">
+      <li>Grape 🍇</li>
+      <li>Mango 🥭</li>
+      <li>Apple 🍎</li>
+      <li>Orange 🍊</li>
+      </ul>
     </div>
   );
 }
